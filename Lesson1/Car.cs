4a242adcbc2b70f65c325302aab73a80731a6aa4
Kate <waykaty99@gmail.com>
2018-10-20 21:07:18 +0300

    Сделали абстрактным класс машина и добавили первый пример машины Nissan

--- Lesson1/Car.cs
+++ Lesson1/Car.cs
@@ -1,15 +1,10 @@
 namespace Lesson1
 {
-    public class Car
+    public abstract class Car
     {
-        private string _name;
-        private int _year;
-
-        public Car(string name, int year)
-        {
-            _name = name;
-            _year = year;
-        }
+        protected string _name;
+        protected int _year;
+        protected string _beepSound;
 
         public string GetName()
         {
@@ -20,5 +15,7 @@ namespace Lesson1
         {
             return _year;
         }
+
+        public abstract void MakeBeep();
     }
 }
--- Lesson1/Nissan.cs
+++ Lesson1/Nissan.cs
@@ -1,7 +1,19 @@
+using System;
+
 namespace Lesson1
 {
-    public class Nissan
+    public class Nissan : Car
     {
-        
+        public Nissan(int year, string beepSound)
+        {
+            _name = "Nissan";
+            _year = year;
+            _beepSound = beepSound;
+        }
+
+        public override void MakeBeep()
+        {
+            Console.Out.WriteLine(_name + ", " + _beepSound);
+        }
     }
 }
