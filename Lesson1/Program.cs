57ff27042b00c56bb00efae898c1350a1b856d36
IdeoG <ideogaib@gmail.com>
2018-10-31 21:20:55 +0300

    some refactor

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,32 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Security;
-
-namespace Lesson1
+﻿namespace Lesson1
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            var nissan = new Nissan(2000, "beeep","xray","red",107,"auto");
+            var nissan = new Nissan(2000, "beeep", "xray", "red", 107, "auto");
             nissan.MakeBeep();
             nissan.GetModel();
             nissan.GetPower();
             nissan.GetColor();
             nissan.GetTransmision();
-            var huynday = new Hyundai(2090, "beeep beep","santa fe","black",125,"manual");
-            huynday.MakeBeep();
-            huynday.GetModel();
-            huynday.GetPower();
-            huynday.GetColor();
-            huynday.GetTransmision();
-            var skoda = new Skoda(2078, "gay","yety","white",209,"auto");
+
+            var hyundai = new Hyundai(2090, "beeep beep", "santa fe", "black", 125, "manual");
+            hyundai.MakeBeep();
+            hyundai.GetModel();
+            hyundai.GetPower();
+            hyundai.GetColor();
+            hyundai.GetTransmision();
+
+            var skoda = new Skoda(2078, "gay", "yety", "white", 209, "auto");
             skoda.MakeBeep();
             skoda.GetModel();
             skoda.GetPower();
             skoda.GetColor();
             skoda.GetTransmision();
-            
         }
     }
 }
